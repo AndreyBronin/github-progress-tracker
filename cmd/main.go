0eb2825dc8a4dfff9121136692c50fe365fedc62
Andrey Bronin <jonnib@yandex.ru>
2018-10-06 20:16:05 +0300

    initial cobra cli provides only usage and version info

--- cmd/main.go
+++ cmd/main.go
@@ -1,47 +1,31 @@
+/*
+ *    Copyright © 2018 Andrey Bronin <jonnib@yandex.ru>.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+/*
+Package main provides CLI interface based on Cobra and Viper
+ */
 package main
 
 import (
-	"github.com/AndreyBronin/github-progress-tracker/tracker"
+	log "github.com/sirupsen/logrus"
 )
 
-
 func main() {
-	collector, _ := tracker.NewGithubTracker()
-
-
-	collector.ProcessRepo("bitcoin", "bitcoin")
-
-
-	/*
-	opt := &github.RepositoryListByOrgOptions{Type: "public"}
-	repos, _, err := client.Repositories.ListByOrg(context.Background(), "insolar", opt)
+	err := rootCmd.Execute()
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
-
-	for _, r := range repos {
-		fmt.Println(*r.Name)
-	}
-
-	events, _ , err := client.Activity.ListRepositoryEvents(context.Background(), "insolar", "insolar", &github.ListOptions{0, 500})
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-
-	for _, e := range events {
-		fmt.Println(e.GetCreatedAt(), e.GetType(), string(*e.RawPayload))
-	}
-*/
-
-	//refs, _, _ := collector.client.Git.ListRefs(context.Background(), "insolar", "insolar", &github.ReferenceListOptions{ListOptions: github.ListOptions{0, 500}})
-	//for _, r := range refs {
-	//	fmt.Println(r.GetObject().GetType())
-	//}
-/*
-	err := collector.ProcessOrganizationRepos("insolar", []string{"insolar"})
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-*/
-	//client.Repositories.ListContributors()
 }
